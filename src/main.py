8fc6e54cb10abac0cc841b3f62507cfa967e904a
wamuumu <matteo68.mc@gmail.com>
2025-12-04 12:30:08 +0100

    feat: updated linear system prompt and added structured schema supporting transitions

--- src/main.py
+++ src/main.py
@@ -4,10 +4,10 @@ from agents.google import GeminiAgent
 from workflows.models.linear import LinearWorkflow
 from workflows.runner import WorkflowRunner
 from tools.macro import MACRO_TOOLS
-from utils.constants import SYSTEM_PROMPT, USER_PROMPT
+from utils.constants import LINEAR_SYSTEM_PROMPT, USER_PROMPT
 
 agent = GeminiAgent(tools=MACRO_TOOLS)
-workflow = agent.generate(SYSTEM_PROMPT, USER_PROMPT, response_model=LinearWorkflow, debug=True)
+workflow = agent.generate(LINEAR_SYSTEM_PROMPT, USER_PROMPT, response_model=LinearWorkflow, debug=True)
 
 print("\nGenerated Workflow:")
 print(json.dumps(workflow.model_dump(), indent=2))
--- src/workflows/models/structured.py
+++ src/workflows/models/structured.py
@@ -1,8 +1,55 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import List, Union, Literal, Optional
 
-class StructuredWorkflow(BaseModel):
-    """A linear workflow consisting of a sequence of steps."""
+class Parameter(BaseModel):
+    """An object representing a parameter item in a workflow step."""
+    key: str
+    value: Union[str, int, float, bool]
+    from_step: Optional[str] = None
+
+class Result(BaseModel):
+    """An object representing a result item in a workflow step."""
+    key: str
+    description: str
+
+class ToolAction(BaseModel):
+    """An action to call a tool."""
+    action: Literal["call_tool"]
+    tool_name: str
+
+class LLMAction(BaseModel):
+    """An action to call a LLM."""
+    action: Literal["call_llm"]
+
+class HumanAction(BaseModel):
+    """An action to involve a human."""
+    action: Literal["human_input"]
+    instructions: str
+
+class GlobalTransitions(BaseModel):
+    rerun: Literal["default_rerun_logic"]
+    human_needed: Literal["default_human_logic"]
+
+class StepTransitions(BaseModel):
+    """A transition from one step to another in a structured workflow."""
+    success: str
+    failure: str
+    others: Optional[GlobalTransitions] = None
+
+class Step(BaseModel):
+    """A single step in a structured workflow."""
+    id: str
     title: str
     description: str
-    # TODO: Implment
+    task: Union[ToolAction, LLMAction, HumanAction]
+    parameters: Optional[List[Parameter]] = None
+    results: List[Result]
+    transitions: StepTransitions # ? Should we implement complex conditional logic
+    thoughts: str
+
+class StructuredWorkflow(BaseModel):
+    """A structured workflow consisting of a sequence of steps and allowing branching."""
+    title: str
+    description: str
+    global_transitions: GlobalTransitions
+    steps: List[Step]
